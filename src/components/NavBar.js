39443ba0de4a75c1983ac9ffc58e9a4f7b38a736
loligop <rodrigocoppa@gmail.com>
2022-08-23 00:07:10 -0300

    Agrego el navbar para la entrega.

---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <a href="inicio.html">Inicio</a>
+                </li>
+                <li>
+                    <a href="inicio.html">Catálogo</a>
+                </li>
+                <li>
+                    <a href="inicio.html">Productos</a>
+                </li>
+                <li>
+                    <a href="inicio.html">Carrito</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+export default NavBar;
